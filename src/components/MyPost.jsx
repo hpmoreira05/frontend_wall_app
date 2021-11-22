caaa22e37f93bbda02a02dda51673e6c78305164
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-22 14:54:55 -0300

    Create MyPost structure

--- src/components/MyPost.jsx
+++ src/components/MyPost.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
-function MyPost() {
+function MyPost({myPost}) {
   return (
-    <div>My post</div>
+    <div>
+      <div>{myPost.title}</div>
+      <div>{myPost.description}</div>
+      <div>{myPost.createdAt}</div>
+      <div>{myPost.editedAt}</div>
+      <button type="button">Edit</button>
+      <button type="button">Delete</button>
+    </div>
   );
 }
 
