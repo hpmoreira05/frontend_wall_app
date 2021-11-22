83308dacbaa73b1e0f4495141d929317f84642a4
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-22 12:03:55 -0300

    Add posts route

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/login';
+import Posts from './pages/posts';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
+      <Route exact path="/posts" component={Posts} />
     </Switch>
   );
 }
