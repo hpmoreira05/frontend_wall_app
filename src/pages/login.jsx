6d5f9de920dee10db20762365630d5a752d9d9b4
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-23 18:55:24 -0300

    Add styte to login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,13 +3,18 @@ import Header from '../components/Header';
 import Register from '../components/Register';
 import SignIn from '../components/SignIn';
 import AppContext from '../context/AppContext';
+import MainImage from '../images/registerImg.svg';
+import styles from '../styles/login.module.css';
 
 function Login() {
   const { signInRendering } = useContext(AppContext);
   return (
     <div>
       <Header />
-      {signInRendering ? <SignIn /> : <Register />}
+      <div className={styles.login}>
+        <img src={MainImage} alt="wall app mobile version" />
+        {signInRendering ? <SignIn /> : <Register />}
+      </div>
     </div>
   );
 }
