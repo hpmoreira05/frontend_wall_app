65ccd2e2de9c9ce4e4c44505dacf72218fac96fa
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-22 21:52:49 -0300

    Add update structure

--- src/components/MyPost.jsx
+++ src/components/MyPost.jsx
@@ -1,18 +1,22 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import DeletePostButton from './DeletePostButton';
 
 function MyPost({ myPost }) {
   const {
     _id, title, description, createdAt, editedAt,
   } = myPost;
+
+  const history = useHistory();
+
   return (
     <div>
       <div>{title}</div>
       <div>{description}</div>
       <div>{createdAt}</div>
       <div>{editedAt}</div>
-      <button type="button">Edit</button>
+      <button type="button" onClick={() => history.push({ pathname: '/updatepost', state: { myPost } })}>Edit</button>
       <DeletePostButton id={_id} />
     </div>
   );
--- src/pages/updatePost.jsx
+++ src/pages/updatePost.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 
-function UpdatePost() {
+function UpdatePost(props) {
+  const { location } = props;
+  const [updatedTitle, setUpdatedTitle] = useState(location.state.myPost.title);
+  const [updatedDescription, setUpdatedDescription] = useState(location.state.myPost.description);
+
   return (
-    <div>Update Post</div>
+    <div>
+      <div>
+        <label htmlFor="title">
+          Title
+          <input
+            value={updatedTitle}
+            type="text"
+            id="title"
+            onChange={(e) => setUpdatedTitle(e.target.value)}
+          />
+        </label>
+        <label htmlFor="description">
+          Password
+          <textarea
+            value={updatedDescription}
+            rows="20"
+            cols="100"
+            id="description"
+            onChange={(e) => setUpdatedDescription(e.target.value)}
+          />
+        </label>
+        {/* <button
+          type="button"
+          onClick={() => fetchCreatePost()}
+        >
+          {isLoading ? 'Loading...' : 'Send'}
+        </button>
+        <button type="button" onClick={() => history.push('/posts')}>Cancel</button> */}
+      </div>
+    </div>
   );
 }
 
