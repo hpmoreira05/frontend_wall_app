f451e759861695a1fc4f85377301f9f5cd449e7e
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-26 16:19:38 -0300

    Add verification in page not found

--- src/pages/pageNotFound.jsx
+++ src/pages/pageNotFound.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Universe from '../images/universe.svg';
 import Header from '../components/Header';
+import AppContext from '../context/AppContext';
+import { validation } from '../service/api';
 
 function PageNotFound() {
+  const { isLogged, setUser, setIsLogged } = useContext(AppContext);
   const history = useHistory();
+
+  const userValidation = async () => {
+    if (!isLogged) {
+      const response = await validation();
+      if (response.status === 200) {
+        setUser(response.message);
+        setIsLogged(true);
+        return;
+      }
+      setIsLogged(false);
+    }
+  };
+
+  useEffect(() => {
+    userValidation();
+  }, []);
+
   return (
     <div>
       <Header />
