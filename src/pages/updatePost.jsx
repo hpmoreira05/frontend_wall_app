edd9e6dae2fb7bdd393db043333810f0d3cadadc
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-26 16:11:24 -0300

    Add verification in update post page

--- src/pages/updatePost.jsx
+++ src/pages/updatePost.jsx
@@ -1,11 +1,29 @@
 /* eslint-disable react/prop-types */
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import NotLogged from '../components/NotLogged';
 import EditPost from '../components/EditPost';
+import { validation } from '../service/api';
 
 function UpdatePost(props) {
-  const { isLogged } = useContext(AppContext);
+  const { isLogged, setUser, setIsLogged } = useContext(AppContext);
+
+  const userValidation = async () => {
+    if (!isLogged) {
+      const response = await validation();
+      if (response.status === 200) {
+        setUser(response.message);
+        setIsLogged(true);
+        return;
+      }
+      setIsLogged(false);
+    }
+  };
+
+  useEffect(() => {
+    userValidation();
+  }, []);
+
   return (
     <div>
       {!isLogged ? <NotLogged /> : <EditPost props={props} />}
